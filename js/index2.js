9586ec83556cb113c01bfa7da5dd7406b72b46a2
Lucas Backes <lucasbackes@gmail.com>
2023-06-22 13:25:43 -0300

    d

--- js/index2.js
+++ js/index2.js
@@ -5,23 +5,23 @@ let el = document.querySelector(".cards--cursos");
 let x = 0, y = 0, top1 = 0, left = 0;
 
 let draggingFunction = (e) => {
-    document.addEventListener('pointercancel', () => {
-        document.removeEventListener("pointermove", draggingFunction);
+    document.addEventListener('mouseup', () => {
+        document.removeEventListener("mousemove", draggingFunction);
     });
 
     el.scrollLeft = left - e.pageX + x;
     el.scrollTop = top1 - e.pageY + y;
 };
 
-el.addEventListener('pointerdown', (e) => {
-    // e.preventDefault();
+el.addEventListener('mousedown', (e) => {
+    e.preventDefault();
 
     y = e.pageY;
     x = e.pageX;
     top1 = el.scrollTop;
     left = el.scrollLeft;
 
-    document.addEventListener('pointermove', draggingFunction);
+    document.addEventListener('mousemove', draggingFunction);
 });
 
 
