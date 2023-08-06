9662d013a09361cff9b63ea7046eb4b6f48465be
lucasbackes <lucasbackes@gmail.com>
2023-08-06 18:45:22 -0300

    Criacao da página de serviços

---
+++ app/js/meus-servicos.js
@@ -0,0 +1,20 @@
+$('#cabecalho').load('cabecalho.html');
+
+$(document).ready(function () {
+
+    console.log('carregou jq');
+  
+
+    $('#cabecalho').on('click', '.menu--icon', function () {
+        console.log('click menu--icon');
+        $('#cabecalho').addClass('mobile--open');
+    });
+
+    $('#cabecalho').on('click', '.close--mobile--menu', function () {
+        $('#cabecalho').removeClass('mobile--open');
+        console.log('tchau');
+    });
+
+       
+
+});
